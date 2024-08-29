bc727fbb108d1e65fb64c00c45896a2efd156ed3
abi-le <abigail.le001@albright.edu>
2024-08-29 10:27:07 -0400

    first commit

---
+++ 01_getting_started/01_03_infinite_skills.py
@@ -0,0 +1,9 @@
+#Infinite skills
+
+'''
+1) I would build a program that allows you to create emojis that describe exactly how you are feeling by a description. You would have a little search bar that you can put in descriptive words, and then an emoji would be generated to fit the descriptive words.
+  
+2) A blocker for social media platforms that parents can use so that their kids are not exposed to things they should not see at a young age. The program would sift through all the media, pictures, videos, etc on the kids account. 
+     
+3) Also maybe a game of some sort, not sure about details, but I ran out of ideas. '''
+
